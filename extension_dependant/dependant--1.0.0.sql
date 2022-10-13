55cea9dc31be3fb681d3f8f001404a567552fbad
Zacharias Knudsen <z@chari.as>
2022-10-13 12:09:49 +0200

    Test

--- extension_dependant/dependant--1.0.0.sql
+++ extension_dependant/dependant--1.0.0.sql
@@ -1,4 +1,7 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION dependant" to load this file. \quit
+
 CREATE FUNCTION dependant(integer)
-  RETURNS integer
-  AS 'dependant'
-  LANGUAGE C IMMUTABLE STRICT;
+    RETURNS integer
+    AS 'dependant'
+    LANGUAGE C IMMUTABLE STRICT;
