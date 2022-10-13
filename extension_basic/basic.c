987b8b5358a03e0b9a1d21b9a80adf036822a0e4
Zacharias Knudsen <z@chari.as>
2022-10-13 13:15:20 +0200

    wip

--- extension_basic/basic.c
+++ extension_basic/basic.c
@@ -1,6 +1,8 @@
 #include <postgres.h> // Primary include file for PostgreSQL server .c files
 #include <fmgr.h>     // Definitions for the Postgres function manager and function-call interface
 
+#include <utils/elog.h>
+
 PG_MODULE_MAGIC;
 
 PGDLLEXPORT Datum basic(PG_FUNCTION_ARGS);
@@ -9,5 +11,8 @@ PG_FUNCTION_INFO_V1(basic);
 
 Datum basic(PG_FUNCTION_ARGS) {
   int arg = PG_GETARG_INT32(0);
+
+  elog(LOG, "basic called");
+
   PG_RETURN_INT32(arg);
 }
