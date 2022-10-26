02972e2a4f72330d2fbb4be5019eb218356cb73f
Zacharias Knudsen <z@chari.as>
2022-10-26 13:52:34 +0200

    Allow different extension name and library name

--- cmake/AddPostgreSQLExtension.cmake
+++ cmake/AddPostgreSQLExtension.cmake
@@ -6,45 +6,52 @@ find_program(PostgreSQL_REGRESS pg_regress
 )
 
 # Helper command to add extensions
-function(PostgreSQL_add_extension NAME)
+function(PostgreSQL_add_extension LIBRARY_NAME)
   set(options RELOCATABLE)
-  set(oneValueArgs COMMENT)
+  set(oneValueArgs NAME COMMENT)
   set(multiValueArgs SOURCES DATA)
   cmake_parse_arguments(EXTENSION "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+  # Default extension name to same as library name
+  if(NOT EXTENSION_NAME)
+    set(EXTENSION_NAME ${LIBRARY_NAME})
+  endif()
+
+  # Allow extensions without sources
   if(EXTENSION_SOURCES)
     # Add extension as a dynamically linked library
-    add_library(${NAME} MODULE ${EXTENSION_SOURCES})
+    add_library(${LIBRARY_NAME} MODULE ${EXTENSION_SOURCES})
 
     # Link extension to PostgreSQL
-    target_link_libraries(${NAME} PostgreSQL::PostgreSQL)
+    target_link_libraries(${LIBRARY_NAME} PostgreSQL::PostgreSQL)
 
-    # fix apple missing symbols
+    # Fix apple missing symbols
     if(APPLE)
-      set_target_properties(${NAME} PROPERTIES LINK_FLAGS ${PostgreSQL_LINK_FLAGS})
+      set_target_properties(${LIBRARY_NAME} PROPERTIES LINK_FLAGS ${PostgreSQL_LINK_FLAGS})
     endif()
 
     # Final touches on output file
-    set_target_properties(${NAME} PROPERTIES
+    set_target_properties(${LIBRARY_NAME} PROPERTIES
+      OUTPUT_NAME ${EXTENSION_NAME}
       INTERPROCEDURAL_OPTIMIZATION TRUE
       #C_VISIBILITY_PRESET hidden # @TODO: how to get this working?
       PREFIX "" # Avoid lib* prefix on output file
     )
 
     # Install .so/.dll to pkglib-dir
-    install(TARGETS ${NAME} LIBRARY DESTINATION "${PostgreSQL_PKG_LIBRARY_DIR}")
+    install(TARGETS ${LIBRARY_NAME} LIBRARY DESTINATION "${PostgreSQL_PKG_LIBRARY_DIR}")
   endif()
 
   # Generate .control file
   configure_file(
     ${CMAKE_SOURCE_DIR}/cmake/control.in
-    ${NAME}.control
+    ${EXTENSION_NAME}.control
   )
 
   # Install everything else into share-dir
   install(
     FILES
-      ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.control
+      ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME}.control
       ${EXTENSION_DATA}
     DESTINATION "${PostgreSQL_SHARE_DIR}/extension"
   )
